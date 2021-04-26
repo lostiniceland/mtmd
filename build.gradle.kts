eafd05d2cf0c90bd2f80eba2ec01843dcb4094bf
Marc Schlegel <marc.schlegel@gmx.de>
2021-04-26 15:40:28 +0200

    Adding Asciidoc PDF Generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins{
     id("base")
+    id("org.asciidoctor.jvm.pdf") version "3.3.2"
 }
 allprojects {
     repositories {
@@ -7,3 +8,15 @@ allprojects {
         mavenCentral()
     }
 }
+
+
+tasks {
+    withType<org.asciidoctor.gradle.jvm.pdf.AsciidoctorPdfTask>{
+        sourceDir(layout.projectDirectory)
+        sources{
+            include("README.adoc")
+        }
+        setOutputDir(layout.projectDirectory)
+    }
+}
+
