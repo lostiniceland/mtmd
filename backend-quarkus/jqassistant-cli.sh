bf46c9e45c6f91e324d1ac613fa392cf984a5655
Marc Schlegel <marc.schlegel@gmx.de>
2021-07-13 10:24:50 +0200

    Make JQAssistant wrapper script parameterized and remove hard-coded path.

--- backend-quarkus/jqassistant-cli.sh
+++ backend-quarkus/jqassistant-cli.sh
@@ -1,3 +1,2 @@
 # Currently JQAssistant and Gradle 7 do not work together easily. Use the CLI distribution
-# At the moment the path is hard-coded
-/home/marc/Development/tools/jqassistant-commandline-neo4jv3-1.10.0/bin/jqassistant.sh analyze report -f build/quarkus-app/app -storeUri file:build/jqassistant -r jqassistant -reportDirectory build/jqassistant/report
+jqassistant "$@" -f build/quarkus-app/app -storeUri file:build/jqassistant -r jqassistant -reportDirectory build/jqassistant/report
